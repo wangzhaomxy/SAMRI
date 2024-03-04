99ea249ee19a74db82731a027154cded2d06e0ab
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-03-04 22:17:08 +1000

    Setup some permanent variables

---
+++ utils/utils.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Permanent variables, universal functions, ect
+"""
+
+IMAGE_PATH = " "
+IMAGE_KEYS = "*T2_img*"  # The image file names containing letters between *
+MASK_KEYS = "*A_seg*"   # The mask file names containing letters between *
+
+
+MODEL_TYPE = "vit_h"
+SAM_CHECKPOINT = "sam_vit_h_4b8939.pth"
+DEVICE = "cuda"
