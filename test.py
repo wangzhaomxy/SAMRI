9f48c8344ef7ba1cdc77268f35cf39719834839e
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-03-06 00:09:40 +1000

    recover the test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 """
 Inference of the MRI-SAM on the nifti datasets.
 """
@@ -16,7 +16,10 @@ from utils.utils import *
 from torch.utils.data import DataLoader
 from utils.dataloader import NiiDataset
 from utils.prompt import *
+from tqdm import tqdm
+from utils.losses import dice_similarity
 
+"""
 # load args from command line
 args = test_parser.parse_args()
 
@@ -24,7 +27,14 @@ device = args.device
 checkpoint=args.checkpoint
 encoder_tpye = args.encoder_tpye
 
-# regist the MRI-SAM model
+# Use args in the formal publics
+"""
+
+encoder_tpye = ENCODER_TYPE
+checkpoint = SAM_CHECKPOINT
+device = DEVICE
+
+# regist the MRI-SAM model and predictor.
 mri_sam_model = sam_model_registry[encoder_tpye](checkpoint)
 mri_sam_model = mri_sam_model.to(device)
 predictor = SamPredictor(mri_sam_model)
@@ -33,9 +43,60 @@ predictor = SamPredictor(mri_sam_model)
 file_path = IMAGE_PATH
 test_dataset = NiiDataset(file_path)
 
-for image, mask in test_dataset:
+# setup essential parameters.
+num_points = NUM_POINTS
+num_bboxes = NUM_BBOXES
+jitter = JITTER
+record = {}
+save_path = SAVE_PATH
+
+for image, mask in tqdm(test_dataset):
     # Image embedding inference
     predictor.set_image(image)
-
+    
+    name = test_dataset.get_name()
+    # split the multi-labeled mask into single labeled
+    # logit masks.
     masks = MaskSplit(mask)
+
+    sub_record = {"p":[], "b":[]}
+    p_rec = []
+    b_rec = []    
+    if num_points == num_bboxes == 1:
+        for each_label_mask in masks: # shape is HW=(255, 255)
+            # generate prompts
+            point = gen_points(each_label_mask)
+            point_lable = np.array([1])
+            bbox = gen_bboxes(each_label_mask, jitter=jitter)
+
+            # generate mask
+            pre_mask_p, _, _ = predictor.predict(
+                                point_coords=point,
+                                point_labels=point_lable,
+                                multimask_output=False,
+                            )
+            
+            pre_mask_b, _, _ = predictor.predict(
+                                point_coords=None,
+                                point_labels=None,
+                                box=bbox[None, :],
+                                multimask_output=False,
+                            )
+            
+            p_dice = dice_similarity(each_label_mask, pre_mask_p[0, :, :])
+            b_dice = dice_similarity(each_label_mask, pre_mask_b[0, :, :])
+
+            p_rec.append(p_dice)
+            b_rec.append(b_dice)
+        sub_record["p"].append(p_rec)
+        sub_record["b"].append(b_rec)
+
+    else:
+        pass
+
+    record[name] = sub_record
+    
+    
+
+
 
