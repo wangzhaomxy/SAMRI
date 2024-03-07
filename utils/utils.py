5de921beebc5296ea8ab0fe14e67fbb18a7899d3
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-03-08 02:32:55 +1000

    change the image path

--- utils/utils.py
+++ utils/utils.py
@@ -4,7 +4,8 @@
 Permanent variables, universal functions, ect
 """
 
-IMAGE_PATH = "/home/s4670484/Documents/MSK/T2_preprocessed_resample_scale2_crop_cv_7c_slice_axis_0_class_7_thresh_7/set_1/test/"
+IMAGE_PATH = "/home/s4670484/Documents/MSK/code_test/"
+    # "/home/s4670484/Documents/MSK/T2_preprocessed_resample_scale2_crop_cv_7c_slice_axis_0_class_7_thresh_7/set_1/test/"
 IMAGE_KEYS = "*T2_img*"  # The image file names containing letters between *
 MASK_KEYS = "*A_seg*"   # The mask file names containing letters between *
 
@@ -24,7 +25,7 @@ NUM_POINTS = 1
 NUM_BBOXES = 1
 JITTER = 10
 
-SAVE_PATH = "/home/s4670484/Documents/result/sam_vit_b/dataset1/"
+SAVE_PATH = "/home/s4670484/Documents/result/med_sam/test/"
 
 LABEL_DICTIONARY = {1:"Femur", 2:"Articular Cartilage-F", 3:"Tibia", 4:"Articular Cartilage-T", 5:"Patella", 6:"Articular Cartilage-P"}
 LABEL_LIST = ["Femur", "Articular Cartilage-F", "Tibia", "Articular Cartilage-T", "Patella", "Articular Cartilage-P"]
