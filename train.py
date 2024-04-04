d4ef5d4ef3cafd1834aa3f3ef49f5ac0b1da0571
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-04-05 02:36:51 +1000

    update the whole train file

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ join = os.path.join
 from tqdm import tqdm
 import torch
 import torch.nn as nn
-from segment_anything import sam_model_registry
+from segment_anything import sam_model_registry, SamPredictor
 import torch.nn.functional as F
 from datetime import datetime
 from utils.dataloader import NiiDataset
@@ -19,7 +19,7 @@ from utils.losses import *
 from utils.prompt import *
 from torch.utils.data import random_split
 from model import SAMRI
-
+from train_predictor import TrainSamPredictor
 
 # setup global parameters
 encoder_type = ENCODER_TYPE["vit_b"] # choose one from vit_b and vit_h.
@@ -33,7 +33,7 @@ num_epochs = NUM_EPOCHS
 train_image_path = TRAIN_IMAGE_PATH
 
 wandb.login()
-experiment = wandb.init(
+wandb.init(
     project="SAMRI",
     config={
         "batch_size": batch_size,
@@ -42,15 +42,15 @@ experiment = wandb.init(
     },
 )
 
-
-def gen_batch(image, mask, prompt):
+def gen_batch(mask, prompt):
     masks = MaskSplit(mask)
+    lenth = len(masks)
     for each_mask in masks:
         if prompt == "point":
             each_prompt = gen_points(each_mask)
         if prompt == "bbox":
             each_prompt = gen_bboxes(each_mask)
-        yield (image, each_mask, each_prompt)
+        yield (each_mask, each_prompt, lenth)
 
 def main():
     sam_model = sam_model_registry[encoder_type](checkpoint=checkpoint)
@@ -59,14 +59,14 @@ def main():
         mask_decoder=sam_model.mask_decoder,
         prompt_encoder=sam_model.prompt_encoder,
     ).to(device)
-
-    samri_model.train()
+    train_predictor = TrainSamPredictor(samri_model)
+    val_predictor = SamPredictor(samri_model)
 
     optimizer = torch.optim.Adam(
         samri_model.mask_decoder.parameters()
     )
 
-    dice_loss = MultiClassDiceLoss()
+    dice_loss = MultiClassDiceLoss(num_classes=1)
     bce_loss = nn.BCEWithLogitsLoss(reduction="mean")
 
     #train
@@ -83,27 +83,34 @@ def main():
     prompts = ["point", "bbox"]
     for epoch in range(start_epoch, num_epochs):
         # training part
+        samri_model.train()
         epoch_loss = 0
         for step, (image, mask) in enumerate(tqdm(train_set)):
+            train_predictor.set_image(image)
             sub_loss = 0
             for prompt in prompts:
-                optimizer.zero_grad()
-                b_image, b_mask, b_prompt = gen_batch(image, mask, prompt)
-                image, mask = image.to(device), mask.to(device)
-                if prompt == "point":
-                    y_pred = samri_model(b_image, points=b_prompt)
-                if prompt == "bbox":
-                    y_pred = samri_model(b_image, bbox=b_prompt)
-                loss = dice_loss(y_pred, b_mask) + bce_loss(y_pred, mask)
-                loss.backward()
-                optimizer.step()
-                sub_loss += loss.item()
-            epoch_loss += sub_loss / len(prompts)
+                    for sub_mask, sub_prompt, lenth in gen_batch(mask, prompt):                        
+                        optimizer.zero_grad()
+
+                        if prompt == "point":
+                            y_pred = samri_model(image, points=sub_prompt)
+                        if prompt == "bbox":
+                            y_pred = samri_model(image, bbox=sub_prompt)
+
+                        sub_mask = sub_mask.to(device)
+                        loss = dice_loss(y_pred, sub_mask) + 20 * bce_loss(y_pred, sub_mask)
+                        
+                        loss.backward()
+                        
+                        optimizer.step()
+                        
+                        sub_loss += loss.item()
+            epoch_loss += sub_loss / (len(prompts)*lenth)
             iter_num += 1
 
         epoch_loss /= step
         losses.append(epoch_loss)
-        wandb.log({"epoch_loss": epoch_loss})
+        wandb.log({"train_epoch_loss": epoch_loss})
         print(
             f'Time: {datetime.now().strftime("%Y%m%d-%H%M")}, Epoch: {epoch}, Loss: {epoch_loss}'
         )
@@ -116,19 +123,36 @@ def main():
         torch.save(checkpoint, join(model_save_path, "samri_model_latest.pth"))
         
         # validation part
+        samri_model.eval()
         val_loss = 0
-        for image, mask in val_set:
+        with torch.no_grad():
+            for step, (vimage, vmask) in enumerate(val_set):
+                val_predictor.set_image(image)
+                val_sub_loss = 0
+                for prompt in prompts:
+                        for vsub_mask, vsub_prompt, lenth in gen_batch(vmask, prompt):
+                            if prompt == "point":
+                                y_pred = samri_model(vimage, points=vsub_prompt)
+                            if prompt == "bbox":
+                                y_pred = samri_model(vimage, bbox=vsub_prompt)
+                            sub_mask = sub_mask.to(device)
+                            val_loss = dice_loss(y_pred, vsub_mask) + 20 * bce_loss(y_pred, sub_mask)
 
-            pass
-            ## save the best model
-            if epoch_loss < best_loss:
-                best_loss = epoch_loss
-                checkpoint = {
-                    "model": samri_model.state_dict(),
-                    "optimizer": optimizer.state_dict(),
-                    "epoch": epoch,
-                }
-                torch.save(checkpoint, join(model_save_path, "mri_sam_model_best.pth"))
+                            val_sub_loss += val_loss.item()
+                val_loss += sub_loss / (len(prompts)*lenth)
+                iter_num += 1
+
+        val_loss /= step
+        wandb.log({"val_epoch_loss": val_loss})
+        ## save the best model
+        if val_loss < best_loss:
+            best_loss = val_loss
+            checkpoint = {
+                "model": samri_model.state_dict(),
+                "optimizer": optimizer.state_dict(),
+                "epoch": epoch,
+            }
+            torch.save(checkpoint, join(model_save_path, "mri_sam_model_best.pth"))
 
 
 if __name__ == "__main__":
