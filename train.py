051f56a30fbfefeb270e201cf9b758edbae0f376
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-04-05 00:55:02 +1000

    move model part to an individual model.py file

--- train.py
+++ train.py
@@ -4,307 +4,132 @@ train the mask decoder
 freeze image encoder and prompt encoder
 """
 
-
-import numpy as np
-import matplotlib.pyplot as plt
 import os
-
 join = os.path.join
 from tqdm import tqdm
-from skimage import transform
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
-import monai
 from segment_anything import sam_model_registry
 import torch.nn.functional as F
-import argparse
-import random
 from datetime import datetime
-import shutil
-import glob
+from utils.dataloader import NiiDataset
+import wandb
+from utils.utils import *
+from utils.losses import *
+from utils.prompt import *
+from torch.utils.data import random_split
+from model import SAMRI
 
 
-class NpyDataset(Dataset):
-    def __init__(self, data_root, bbox_shift=20):
-        self.data_root = data_root
-        self.gt_path = join(data_root, "gts")
-        self.img_path = join(data_root, "imgs")
-        self.gt_path_files = sorted(
-            glob.glob(join(self.gt_path, "**/*.npy"), recursive=True)
-        )
-        self.gt_path_files = [
-            file
-            for file in self.gt_path_files
-            if os.path.isfile(join(self.img_path, os.path.basename(file)))
-        ]
-        self.bbox_shift = bbox_shift
-        print(f"number of images: {len(self.gt_path_files)}")
+# setup global parameters
+encoder_type = ENCODER_TYPE["vit_b"] # choose one from vit_b and vit_h.
+checkpoint = SAM_CHECKPOINT[encoder_type]
+batch_size = BATCH_SIZE
+data_path = TRAIN_IMAGE_PATH
+timestamp = datetime.now().strftime("%Y%m%d-%H%M")
+model_save_path = MODEL_SAVE_PATH
+device = DEVICE
+num_epochs = NUM_EPOCHS
+train_image_path = TRAIN_IMAGE_PATH
 
-    def __len__(self):
-        return len(self.gt_path_files)
-
-    def __getitem__(self, index):
-        # load npy image (1024, 1024, 3), [0,1]
-        img_name = os.path.basename(self.gt_path_files[index])
-        img_1024 = np.load(
-            join(self.img_path, img_name), "r", allow_pickle=True
-        )  # (1024, 1024, 3)
-        # convert the shape to (3, H, W)
-        img_1024 = np.transpose(img_1024, (2, 0, 1))
-        assert (
-            np.max(img_1024) <= 1.0 and np.min(img_1024) >= 0.0
-        ), "image should be normalized to [0, 1]"
-        gt = np.load(
-            self.gt_path_files[index], "r", allow_pickle=True
-        )  # multiple labels [0, 1,4,5...], (256,256)
-        assert img_name == os.path.basename(self.gt_path_files[index]), (
-            "img gt name error" + self.gt_path_files[index] + self.npy_files[index]
-        )
-        label_ids = np.unique(gt)[1:]
-        gt2D = np.uint8(
-            gt == random.choice(label_ids.tolist())
-        )  # only one label, (256, 256)
-        assert np.max(gt2D) == 1 and np.min(gt2D) == 0.0, "ground truth should be 0, 1"
-        y_indices, x_indices = np.where(gt2D > 0)
-        x_min, x_max = np.min(x_indices), np.max(x_indices)
-        y_min, y_max = np.min(y_indices), np.max(y_indices)
-        # add perturbation to bounding box coordinates
-        H, W = gt2D.shape
-        x_min = max(0, x_min - random.randint(0, self.bbox_shift))
-        x_max = min(W, x_max + random.randint(0, self.bbox_shift))
-        y_min = max(0, y_min - random.randint(0, self.bbox_shift))
-        y_max = min(H, y_max + random.randint(0, self.bbox_shift))
-        bboxes = np.array([x_min, y_min, x_max, y_max])
-        return (
-            torch.tensor(img_1024).float(),
-            torch.tensor(gt2D[None, :, :]).long(),
-            torch.tensor(bboxes).float(),
-            img_name,
-        )
-    
-
-# set up parser
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-p",
-    "--train_path",
-    type=str,
-    default="/scratch/itee/uqzwan40/dataset/MSK/",
-    help="path to training nii files; two subfolders: train and test",
+wandb.login()
+experiment = wandb.init(
+    project="SAMRI",
+    config={
+        "batch_size": batch_size,
+        "data_path": data_path,
+        "model_type": encoder_type,
+    },
 )
-parser.add_argument("-task_name", type=str, default="MRI-SAM-ViT-h")
-parser.add_argument("-model_type", type=str, default="vit_h")
-parser.add_argument(
-    "-checkpoint", type=str, default="work_dir/SAM/sam_vit_b_01ec64.pth"
-)
-# parser.add_argument('-device', type=str, default='cuda:0')
-parser.add_argument(
-    "--load_pretrain", type=bool, default=True, help="use wandb to monitor training"
-)
-parser.add_argument("-pretrain_model_path", type=str, default="")
-parser.add_argument("-work_dir", type=str, default="/scratch/itee/uqzwan40/checkpoints/SAM/sam_vit_h_4b8939.pth")
-# train
-parser.add_argument("-num_epochs", type=int, default=1000)
-parser.add_argument("-batch_size", type=int, default=4)
-parser.add_argument("-num_workers", type=int, default=1)
-# Optimizer parameters
-parser.add_argument(
-    "-weight_decay", type=float, default=0.01, help="weight decay (default: 0.01)"
-)
-parser.add_argument(
-    "-lr", type=float, default=0.0001, metavar="LR", help="learning rate (absolute lr)"
-)
-parser.add_argument(
-    "-use_wandb", type=bool, default=False, help="use wandb to monitor training"
-)
-parser.add_argument("-use_amp", action="store_true", default=False, help="use amp")
-parser.add_argument(
-    "--resume", type=str, default="", help="Resuming training from checkpoint"
-)
-parser.add_argument("--device", type=str, default="cuda:0")
-args = parser.parse_args()
 
-if args.use_wandb:
-    import wandb
 
-    wandb.login()
-    wandb.init(
-        project=args.task_name,
-        config={
-            "lr": args.lr,
-            "batch_size": args.batch_size,
-            "data_path": args.tr_npy_path,
-            "model_type": args.model_type,
-        },
-    )
-
-# set up model for training
-run_id = datetime.now().strftime("%Y%m%d-%H%M")
-model_save_path = join(args.work_dir, args.task_name + "-" + run_id)
-device = torch.device(args.device)
-
-#set up model
-class MRI_SAM(nn.Module):
-    def __init__(
-        self,
-        image_encoder,
-        mask_decoder,
-        prompt_encoder,
-    ):
-        super().__init__()
-        self.image_encoder = image_encoder
-        self.mask_decoder = mask_decoder
-        self.prompt_encoder = prompt_encoder
-        # freeze prompt encoder
-        for param in self.prompt_encoder.parameters():
-            param.requires_grad = False
-
-    def forward(self, image, box):
-        # do not compute gradients for image encoder and prompt encoder
-        with torch.no_grad():
-            image_embedding = self.image_encoder(image)  # (B, 256, 64, 64)
-            box_torch = torch.as_tensor(box, dtype=torch.float32, device=image.device)
-            if len(box_torch.shape) == 2:
-                box_torch = box_torch[:, None, :]  # (B, 1, 4)
-
-            sparse_embeddings, dense_embeddings = self.prompt_encoder(
-                points=None,
-                boxes=box_torch,
-                masks=None,
-            )
-        low_res_masks, _ = self.mask_decoder(
-            image_embeddings=image_embedding,  # (B, 256, 64, 64)
-            image_pe=self.prompt_encoder.get_dense_pe(),  # (1, 256, 64, 64)
-            sparse_prompt_embeddings=sparse_embeddings,  # (B, 2, 256)
-            dense_prompt_embeddings=dense_embeddings,  # (B, 256, 64, 64)
-            multimask_output=False,
-        )
-        ori_res_masks = F.interpolate(
-            low_res_masks,
-            size=(image.shape[2], image.shape[3]),
-            mode="bilinear",
-            align_corners=False,
-        )
-        return ori_res_masks
-
+def gen_batch(image, mask, prompt):
+    masks = MaskSplit(mask)
+    for each_mask in masks:
+        if prompt == "point":
+            each_prompt = gen_points(each_mask)
+        if prompt == "bbox":
+            each_prompt = gen_bboxes(each_mask)
+        yield (image, each_mask, each_prompt)
 
 def main():
-    os.makedirs(model_save_path, exist_ok=True)
-    shutil.copyfile(
-        __file__, join(model_save_path, run_id + "_" + os.path.basename(__file__))
-    )
-
-    sam_model = sam_model_registry[args.model_type](checkpoint=args.checkpoint)
-    mri_sam_model = MRI_SAM(
+    sam_model = sam_model_registry[encoder_type](checkpoint=checkpoint)
+    samri_model = SAMRI(
         image_encoder=sam_model.image_encoder,
         mask_decoder=sam_model.mask_decoder,
         prompt_encoder=sam_model.prompt_encoder,
     ).to(device)
-    mri_sam_model.train()
 
-    print(
-        "Number of total parameters: ",
-        sum(p.numel() for p in mri_sam_model.parameters()),
-    )
-    print(
-        "Number of trainable parameters: ",
-        sum(p.numel() for p in mri_sam_model.parameters() if p.requires_grad),
-    )
+    samri_model.train()
 
     optimizer = torch.optim.Adam(
-        mri_sam_model.mask_decoder.parameters(), lr=args.lr, weight_decay=args.weight_decay
+        samri_model.mask_decoder.parameters()
     )
 
-    seg_loss = monai.losses.DiceLoss(sigmoid=True, squared_pred=True, reduction="mean")
-    # cross entropy loss
-    ce_loss = nn.BCEWithLogitsLoss(reduction="mean")
+    dice_loss = MultiClassDiceLoss()
+    bce_loss = nn.BCEWithLogitsLoss(reduction="mean")
 
     #train
-    num_epochs = args.num_epochs
     iter_num = 0
     losses = []
     best_loss = 1e10
-    train_dataset = NpyDataset(args.tr_npy_path)
-
-    print("Number of training samples: ", len(train_dataset))
-    train_dataloader = DataLoader(
-        train_dataset,
-        batch_size=args.batch_size,
-        shuffle=True,
-        num_workers=args.num_workers,
-        pin_memory=True,
-    )
+    train_dataset = NiiDataset(train_image_path)
+    n_val = int(len(train_dataset) * 0.1)
+    n_train = len(train_dataset) - n_val
+    train_set, val_set = random_split(train_dataset, [n_train, n_val], 
+                                    generator=torch.Generator().manual_seed(0))
 
     start_epoch = 0
-    if args.resume is not None:
-        if os.path.isfile(args.resume):
-            ## Map model to be loaded to specified single GPU
-            checkpoint = torch.load(args.resume, map_location=device)
-            start_epoch = checkpoint["epoch"] + 1
-            mri_sam_model.load_state_dict(checkpoint["model"])
-            optimizer.load_state_dict(checkpoint["optimizer"])
-    if args.use_amp:
-        scaler = torch.cuda.amp.GradScaler()
-
+    prompts = ["point", "bbox"]
     for epoch in range(start_epoch, num_epochs):
+        # training part
         epoch_loss = 0
-        for step, (image, gt2D, boxes, _) in enumerate(tqdm(train_dataloader)):
-            optimizer.zero_grad()
-            boxes_np = boxes.detach().cpu().numpy()
-            image, gt2D = image.to(device), gt2D.to(device)
-            if args.use_amp:
-                ## AMP
-                with torch.autocast(device_type="cuda", dtype=torch.float16):
-                    medsam_pred = mri_sam_model(image, boxes_np)
-                    loss = seg_loss(medsam_pred, gt2D) + ce_loss(
-                        medsam_pred, gt2D.float()
-                    )
-                scaler.scale(loss).backward()
-                scaler.step(optimizer)
-                scaler.update()
+        for step, (image, mask) in enumerate(tqdm(train_set)):
+            sub_loss = 0
+            for prompt in prompts:
                 optimizer.zero_grad()
-            else:
-                medsam_pred = mri_sam_model(image, boxes_np)
-                loss = seg_loss(medsam_pred, gt2D) + ce_loss(medsam_pred, gt2D.float())
+                b_image, b_mask, b_prompt = gen_batch(image, mask, prompt)
+                image, mask = image.to(device), mask.to(device)
+                if prompt == "point":
+                    y_pred = samri_model(b_image, points=b_prompt)
+                if prompt == "bbox":
+                    y_pred = samri_model(b_image, bbox=b_prompt)
+                loss = dice_loss(y_pred, b_mask) + bce_loss(y_pred, mask)
                 loss.backward()
                 optimizer.step()
-                optimizer.zero_grad()
-
-            epoch_loss += loss.item()
+                sub_loss += loss.item()
+            epoch_loss += sub_loss / len(prompts)
             iter_num += 1
 
         epoch_loss /= step
         losses.append(epoch_loss)
-        if args.use_wandb:
-            wandb.log({"epoch_loss": epoch_loss})
+        wandb.log({"epoch_loss": epoch_loss})
         print(
             f'Time: {datetime.now().strftime("%Y%m%d-%H%M")}, Epoch: {epoch}, Loss: {epoch_loss}'
         )
         ## save the latest model
         checkpoint = {
-            "model": mri_sam_model.state_dict(),
+            "model": samri_model.state_dict(),
             "optimizer": optimizer.state_dict(),
             "epoch": epoch,
         }
-        torch.save(checkpoint, join(model_save_path, "mri_sam_model_latest.pth"))
-        ## save the best model
-        if epoch_loss < best_loss:
-            best_loss = epoch_loss
-            checkpoint = {
-                "model": mri_sam_model.state_dict(),
-                "optimizer": optimizer.state_dict(),
-                "epoch": epoch,
-            }
-            torch.save(checkpoint, join(model_save_path, "mri_sam_model_best.pth"))
+        torch.save(checkpoint, join(model_save_path, "samri_model_latest.pth"))
+        
+        # validation part
+        val_loss = 0
+        for image, mask in val_set:
 
-        # plot loss
-        plt.plot(losses)
-        plt.title("Dice + Cross Entropy Loss")
-        plt.xlabel("Epoch")
-        plt.ylabel("Loss")
-        plt.savefig(join(model_save_path, args.task_name + "train_loss.png"))
-        plt.close()
+            pass
+            ## save the best model
+            if epoch_loss < best_loss:
+                best_loss = epoch_loss
+                checkpoint = {
+                    "model": samri_model.state_dict(),
+                    "optimizer": optimizer.state_dict(),
+                    "epoch": epoch,
+                }
+                torch.save(checkpoint, join(model_save_path, "mri_sam_model_best.pth"))
+
 
 if __name__ == "__main__":
     main()
