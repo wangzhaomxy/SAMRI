15ed32681913907e07cb318dbde219c38db70238
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-04-11 15:48:33 +1000

    change the model save path and the reading path

--- utils/utils.py
+++ utils/utils.py
@@ -6,6 +6,8 @@ Permanent variables, universal functions, ect
 
 pre_path = "/scratch/user/s4670484/" # The main folder of files
 img_path = "MSK/T2_preprocessed_resample_scale2_crop_cv_7c_slice_axis_0_class_7_thresh_7/"
+SAVE_PATH = "/home/s4670484/Documents/"
+MODEL_SAVE_PATH = SAVE_PATH + "cp_temp/"
 
 TRAIN_IMAGE_PATH = [pre_path + img_path + 'set_1/train/',
                     pre_path + img_path + 'set_2/train/',
@@ -29,7 +31,7 @@ ENCODER_TYPE = {"vit_b":"vit_b",
 SAM_CHECKPOINT = {"vit_b":pre_path + "Model_dir/sam_vit_b_01ec64.pth",
                   "vit_h":pre_path + "Model_dir/sam_vit_h_4b8939.pth",
                   "med_sam":pre_path + "Model_dir/medsam_vit_b.pth",
-                  "samri":pre_path + "Model_dir/samri_vitb.pth"
+                  "samri":MODEL_SAVE_PATH
                   }
 
 
@@ -42,8 +44,7 @@ NUM_POINTS = 3
 NUM_BBOXES = 1
 JITTER = 10
 
-SAVE_PATH = "/home/s4670484/Documents/"
-MODEL_SAVE_PATH = SAVE_PATH + "cp_temp/"
+
 
 LABEL_DICTIONARY = {1:"Femur", 2:"Articular Cartilage-F", 3:"Tibia", 4:"Articular Cartilage-T", 5:"Patella", 6:"Articular Cartilage-P"}
 LABEL_LIST = ["Femur", "Articular Cartilage-F", "Tibia", "Articular Cartilage-T", "Patella", "Articular Cartilage-P"]
