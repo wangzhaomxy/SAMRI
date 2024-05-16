beba2ad3a5382970662e37007b28e5adf15e35e9
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-05-16 19:54:58 +1000

    change optimizer to adamw 1e-4, decay 0.01

--- train.py
+++ train.py
@@ -60,13 +60,13 @@ def main():
     ).to(device)
     train_predictor = TrainSamPredictor(samri_model)
 
-    # optimizer = torch.optim.AdamW(
-    #     samri_model.mask_decoder.parameters(),
-    #     lr=1e-4, 
-    #     weight_decay=0.1
-    # )
+    optimizer = torch.optim.AdamW(
+        samri_model.mask_decoder.parameters(),
+        lr=1e-4, 
+        weight_decay=0.01
+    )
 
-    optimizer = torch.optim.Adam(samri_model.mask_decoder.parameters())
+    # optimizer = torch.optim.Adam(samri_model.mask_decoder.parameters())
 
     dice_loss = DiceLoss(sigmoid=True, squared_pred=True, reduction="mean")
 
