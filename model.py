a43f7163d70a8c9da5d8211522dd45610d6cbb7f
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-10-01 13:26:34 +1000

    add build_samri function to build samri model.

--- model.py
+++ model.py
@@ -6,17 +6,21 @@ Reference: The model is referenced from the segment anything model,
             link: https://github.com/facebookresearch/segment-anything
 """
 
+from functools import partial
+from pathlib import Path
+import urllib.request
 import torch
-from torch.nn import functional as F
-import numpy as np
+
+from segment_anything.modeling import (
+    ImageEncoderViT,
+    MaskDecoder,
+    PromptEncoder,
+    Sam,
+    TwoWayTransformer,
+)
 
 from typing import Any, Dict, List
 
-from segment_anything.modeling.image_encoder import ImageEncoderViT
-from segment_anything.modeling.mask_decoder import MaskDecoder
-from segment_anything.modeling.prompt_encoder import PromptEncoder
-
-from segment_anything.modeling import Sam
 # from skimage import exposure
 from torchvision.transforms.functional import equalize
 
@@ -150,3 +154,69 @@ class SAMRI(Sam):
       for img_name, embedding in zip(img_names, image_embeddings):
         torch.save(embedding, output_path+"/"+img_name[:-7]+".pt")
 
+
+def build_samri(checkpoint=None):
+    return _build_samri(
+        encoder_embed_dim=768,
+        encoder_depth=12,
+        encoder_num_heads=12,
+        encoder_global_attn_indexes=[2, 5, 8, 11],
+        checkpoint=checkpoint,
+    )
+
+
+def _build_samri(
+    encoder_embed_dim,
+    encoder_depth,
+    encoder_num_heads,
+    encoder_global_attn_indexes,
+    checkpoint=None,
+):
+    prompt_embed_dim = 256
+    image_size = 1024
+    vit_patch_size = 16
+    image_embedding_size = image_size // vit_patch_size
+    sam = SAMRI(
+        image_encoder=ImageEncoderViT(
+            depth=encoder_depth,
+            embed_dim=encoder_embed_dim,
+            img_size=image_size,
+            mlp_ratio=4,
+            norm_layer=partial(torch.nn.LayerNorm, eps=1e-6),
+            num_heads=encoder_num_heads,
+            patch_size=vit_patch_size,
+            qkv_bias=True,
+            use_rel_pos=True,
+            global_attn_indexes=encoder_global_attn_indexes,
+            window_size=14,
+            out_chans=prompt_embed_dim,
+        ),
+        prompt_encoder=PromptEncoder(
+            embed_dim=prompt_embed_dim,
+            image_embedding_size=(image_embedding_size, image_embedding_size),
+            input_image_size=(image_size, image_size),
+            mask_in_chans=16,
+        ),
+        mask_decoder=MaskDecoder(
+            num_multimask_outputs=3,
+            transformer=TwoWayTransformer(
+                depth=2,
+                embedding_dim=prompt_embed_dim,
+                mlp_dim=2048,
+                num_heads=8,
+            ),
+            transformer_dim=prompt_embed_dim,
+            iou_head_depth=3,
+            iou_head_hidden_dim=256,
+        ),
+        pixel_mean=[123.675, 116.28, 103.53],
+        pixel_std=[58.395, 57.12, 57.375],
+    )
+    sam.eval()
+    checkpoint = Path(checkpoint)
+
+    if checkpoint is not None:
+        with open(checkpoint, "rb") as f:
+            state_dict = torch.load(f)
+        sam.load_state_dict(state_dict)
+    return sam
