477408db46b09e9a7528042c600d02a9d7230fc3
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-10-24 22:51:46 +1000

    test the slurm

--- train_multi_gpus.sh
+++ train_multi_gpus.sh
@@ -1,12 +1,12 @@
 #!/bin/bash --login
-#SBATCH --nodes=3
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32G
 #SBATCH --job-name=SAMRI
 #SBATCH --time=24:00:00
 #SBATCH --partition=gpu_cuda
-#SBATCH --gres=gpu:l40:3
+#SBATCH --gres=gpu:l40:2
 #SBATCH --account=a_auto_mr_disease
 #SBATCH -o /home/s4670484/Documents/slurm-%j.output
 #SBATCH -e /home/s4670484/Documents/slurm-%j.error
