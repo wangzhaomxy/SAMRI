a834f67bd628aed3aea8246dd64bcd89df8bc74c
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-10-24 22:45:01 +1000

    update outputs path

--- train_multi_gpus.sh
+++ train_multi_gpus.sh
@@ -8,8 +8,8 @@
 #SBATCH --partition=gpu_cuda
 #SBATCH --gres=gpu:l40:3
 #SBATCH --account=a_auto_mr_disease
-#SBATCH -o slurm-%j.output
-#SBATCH -e slurm-%j.error
+#SBATCH -o /home/s4670484/Documents/slurm-%j.output
+#SBATCH -e /home/s4670484/Documents/slurm-%j.error
 
 module load anaconda3
 source $EBROOTANACONDA3/etc/profile.d/conda.sh
