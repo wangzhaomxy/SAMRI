11c955cd476dcf11380b52d0e1a6da7c5fc20d93
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-11-01 00:37:14 +1000

    debug by adding prints

--- utils/utils.py
+++ utils/utils.py
@@ -41,7 +41,9 @@ def get_checkpoint(path):
     print("cp_list", cp_list)
     cp_names = [(os.path.basename(cp)[:-4]) for cp in cp_list]
     print("cp_names", cp_names)
-    start_epoch = [int(cp.split('_')[-1]) for cp in cp_names if cp != ""]
+    start_epochs = [int(cp.split('_')[-1]) for cp in cp_names if cp != ""]
+    print(start_epoch)
+    start_epoch = max(start_epochs)
     cp_name = glob(path + f"*_{str(start_epoch)}.pth*")[0]
     print(start_epoch, " and ", cp_name)
     return cp_name, start_epoch
