10f63a2cf535d2be4f95fe7f504d1c6451a04c89
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-10-31 19:14:49 +1000

    modify train_fast as multi GPU training process.

--- train_fast.py
+++ train_fast.py
@@ -18,6 +18,11 @@ from utils.prompt import *
 from model import SAMRI
 from train_predictor import TrainSamPredictor
 import glob
+import torch.multiprocessing as mp
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+
 
 # setup global parameters
 model_type = "samri"
@@ -26,10 +31,10 @@ sam_checkpoint = sorted(glob.glob(MODEL_SAVE_PATH + "*"))[-1]
 batch_size = BATCH_SIZE
 data_path = TRAIN_IMAGE_PATH
 model_save_path = MODEL_SAVE_PATH
-device = DEVICE
 num_epochs = NUM_EPOCHS
 train_image_path = TRAIN_IMAGE_PATH
 train_image_path.remove('/scratch/project/samri/Embedding/totalseg_mr/')
+save_every = 1
 
 def gen_batch(mask, prompt):
     masks = MaskSplit(mask)
@@ -41,14 +46,23 @@ def gen_batch(mask, prompt):
             each_prompt = gen_bboxes(each_mask)
         yield (each_mask, each_prompt, lenth)
 
-def main():
+def main(gpu, world_size, num_epochs, save_every):
+    torch.cuda.set_device(gpu)
+    torch.distributed.init_process_group(backend="nccl", rank=rank, world_size=world_size)
+    
     sam_model = sam_model_registry[encoder_type](sam_checkpoint)
     samri_model = SAMRI(
         image_encoder=sam_model.image_encoder,
         mask_decoder=sam_model.mask_decoder,
         prompt_encoder=sam_model.prompt_encoder,
-    ).to(device)
+    )
     train_predictor = TrainSamPredictor(samri_model)
+
+    samri_model = DDP(samri_model, 
+                        device_ids=[gpu],
+                        gradient_as_bucket_view=True,
+                        find_unused_parameters=True
+                        )
 
     optimizer = torch.optim.AdamW(
         samri_model.mask_decoder.parameters(),
@@ -61,7 +75,7 @@ def main():
     #train
     losses = []
     train_dataset = EmbDataset(train_image_path)
-    train_loader = DataLoader(train_dataset)
+    train_loader = DataLoader(train_dataset, shuffle=False, sampler=DistributedSampler(train_dataset))
 
     start_epoch = int(os.path.basename(sam_checkpoint)[:-4].split('_')[-1])
     prompts = ["point", "bbox"]
@@ -109,11 +123,13 @@ def main():
         # torch.save(samri_model.state_dict(), join(model_save_path, "samri_latest.pth"))
         
         ## save the latest model
-        if (epoch + 1) % 1 == 0:
+        if (epoch + 1) % save_every == 0 and gpu == 0:
             print(f"The {epoch+1} / {num_epochs} epochs,  Loss: {epoch_loss}.")
             torch.save(samri_model.state_dict(), join(model_save_path, f"samri_vitb_{str(epoch+1)}.pth"))
-        
+    destroy_process_group()    
 
 
 if __name__ == "__main__":
+    world_size = torch.cuda.device_count()
+    mp.spawn(main, args=(world_size, num_epochs, save_every), nprocs=world_size)
     main()
