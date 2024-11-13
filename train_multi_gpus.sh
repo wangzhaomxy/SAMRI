302796419b09a01a04f9b16ed56dc2f828e0f425
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-11-13 15:01:11 +1000

    cpus =1

--- train_multi_gpus.sh
+++ train_multi_gpus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --login
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=32G
 #SBATCH --job-name=SAMRI
 #SBATCH --time=96:00:00
