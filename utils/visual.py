8701d90496881489b91dfd271f1e4c977b4f96a3
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-11-21 23:41:28 +1000

    create get_dice_from_ds function to get dice and modify the test_visualize to adapt this function.

--- utils/visual.py
+++ utils/visual.py
@@ -7,6 +7,12 @@ reference: https://github.com/facebookresearch/segment-anything/blob/main/notebo
 
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+from segment_anything import SamPredictor
+from utils.utils import *
+from utils.prompt import *
+from utils.losses import dice_similarity
+
 
 def show_mask(mask, ax, random_color=False):
     if random_color:
@@ -27,4 +33,51 @@ def show_box(box, ax):
     x0, y0 = box[0], box[1]
     w, h = box[2] - box[0], box[3] - box[1]
     ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))    
+
+def get_dice_from_ds(model, test_dataset):
+    """
+    Get point prompt 
+
+    Args:
+        model (SAM model): The SAM model loaded from Checkpoint.
+        test_dataset (Dataset): The pytorch dataset from torch.Dataset.
+
+    Returns:
+        ([p_record], [b_record]): 
+            p_record:A list of DSC of point prompt for the test dataset.
+            b_record:A list of DSC of bbox prompt for the test dataset.
+    """
+    predictor = SamPredictor(model)
+    p_record = []
+    b_record = []
+
+    for image, mask in tqdm(test_dataset):
+        # Image embedding inference
+        predictor.set_image(image)
+        masks = MaskSplit(mask)
+
+        for each_mask in masks:
+            # generate prompts
+            point = gen_points(each_mask)
+            point_label = np.array([1])
+            bbox = gen_bboxes(each_mask, jitter=JITTER)
+
+            # generate mask
+            pre_mask_p, _, _ = predictor.predict(
+                                point_coords=point,
+                                point_labels=point_label,
+                                multimask_output=False,
+                            )
+            
+            pre_mask_b, _, _ = predictor.predict(
+                                point_coords=None,
+                                point_labels=None,
+                                box=bbox[None, :],
+                                multimask_output=False,
+                            )
+
+            # save DSC
+            p_record.append(dice_similarity(each_mask, pre_mask_p[0, :, :]))
+            b_record.append(dice_similarity(each_mask, pre_mask_b[0, :, :]))
+    return p_record, b_record
 
