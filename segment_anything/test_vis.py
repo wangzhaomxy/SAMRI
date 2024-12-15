a2638e81ba8054765fb6261d28d332010fa9e881
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2024-12-15 19:57:17 +1000

    auto test model and save results.

---
+++ segment_anything/test_vis.py
@@ -0,0 +1,38 @@
+from segment_anything import sam_model_registry
+from utils.visual import get_dice_from_ds
+from utils.utils import *
+from utils.dataloader import NiiDataset
+import pickle
+from tqdm import tqdm
+
+
+file_paths = TEST_IMAGE_PATH
+ckpt_list = ["/scratch/project/samri/Model_save/mult/samri_vitb_mult_45.pth"]
+
+def save_test_record(file_paths, sam_model, save_path):
+    p_record = []
+    b_record = []
+    for file_path in file_paths:
+        print("Processing the dataset: ",file_path)
+        test_dataset = NiiDataset([file_path], multi_mask= True)    
+        p_record_vitb, b_record_vitb = get_dice_from_ds(model=sam_model, test_dataset=test_dataset)
+        p_record.append(p_record_vitb)
+        b_record.append(b_record_vitb)
+        final_record = {"p":p_record,"b":b_record}
+    with open(save_path, "wb") as f:
+        pickle.dump(final_record, f)
+
+for ckpt in ckpt_list:
+    model_type = 'vit_b'# Choose one from vit_b, vit_h, samri, and med_sam
+    encoder_tpye = ENCODER_TYPE[model_type] 
+    checkpoint = ckpt
+    device = DEVICE
+    file_name = ckpt.split("/")[-1]
+    print("Testing Check-point " + file_name)
+
+    # regist the MRI-SAM model and predictor.
+    sam_model = sam_model_registry[encoder_tpye](checkpoint)
+    sam_model = sam_model.to(device)
+    save_path = "/scratch/project/samri/Eval_results/" + file_name[:-4]
+
+    save_test_record(file_paths, sam_model, save_path)
