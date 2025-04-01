ae190fd0755923d1873d39b5e613fb2ceff9221d
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2025-04-01 12:40:03 +1000

    debugging training code of batch single gpu with random mask

--- train_in_batch_single_gpu_rand_msk.py
+++ train_in_batch_single_gpu_rand_msk.py
@@ -50,10 +50,10 @@ def main():
 
     optimizer = torch.optim.AdamW(
         samri_model.mask_decoder.parameters(),
-        lr=1e-5, 
+        lr=1e-4, 
         weight_decay=0.1
     )
-    #scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
     
     dice_focal_loass = DiceFocalLoss(sigmoid=True, 
                                      squared_pred=True,
@@ -104,7 +104,7 @@ def main():
 
                 optimizer.zero_grad()
                 epoch_loss += loss.item()
-        #scheduler.step()
+        scheduler.step()
         epoch_loss /= step
         losses.append(epoch_loss)
 
