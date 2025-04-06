0012f43a51110051789dd1599064ef50ff1da7f9
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2025-04-07 00:15:57 +1000

    visualize the medsam with new data format. Add pixel number and area percentage in output.

--- test_vis.py
+++ test_vis.py
@@ -8,8 +8,8 @@ from tqdm import tqdm
 
 file_paths = TEST_IMAGE_PATH
 ckpt_root_path = "/scratch/project/samri/Model_save/"
-model_folder = "box/"
-save_path = "/scratch/project/samri/Eval_results/" + "pixel_count" #+ model_folder
+model_folder = "base/"
+save_path = "/scratch/project/samri/Eval_results/" + model_folder
 # ckpt_list = [ckpt_root_path + model_folder + "samri_vitb_box_0.pth",
 #              ckpt_root_path + model_folder + "samri_vitb_box_1.pth",
 #              ckpt_root_path + model_folder + "samri_vitb_box_2.pth",
@@ -25,34 +25,27 @@ save_path = "/scratch/project/samri/Eval_results/" + "pixel_count" #+ model_fold
 # ckpt_list = ["/scratch/user/s4670484/Model_dir/sam_vit_b_01ec64.pth"]
 ckpt_list = ["/scratch/user/s4670484/Model_dir/medsam_vit_b.pth"]
 
-# def save_pxl_record(file_paths, save_path):
-#     pixel_count, area_percentage = [], []
-#     for file_path in file_paths:
-#         print("Processing the dataset: ",file_path)
-#         test_dataset = NiiDataset([file_path], multi_mask= True)
-#         pixel_count_vit, area_percentage_vit = get_pix_num_from_ds(test_dataset=test_dataset)
-#         pixel_count.append(pixel_count_vit)
-#         area_percentage.append(area_percentage_vit)
-#         final_record = {"pixel_count":pixel_count,"area_percentage":area_percentage}
-#     with open(save_path, "wb") as f:
-#         pickle.dump(final_record, f)
-
-# save_pxl_record(file_paths, save_path)
-# print("Done!")
-
 def save_test_record(file_paths, sam_model, save_path):
     p_record, b_record = [], []
     pixel_count, area_percentage = [], []
     for file_path in file_paths:
         print("Processing the dataset: ",file_path)
         test_dataset = NiiDataset([file_path], multi_mask= True)    
-        p_record_vitb, b_record_vitb = get_dice_from_ds(model=sam_model, test_dataset=test_dataset, med_sam=True)
-        pixel_count_vit, area_percentage_vit = get_pix_num_from_ds(test_dataset=test_dataset)
+        (p_record_vitb, 
+         b_record_vitb, 
+         pixel_count_vit, 
+         area_percentage_vit) = get_dice_from_ds(model=sam_model, 
+                                                 test_dataset=test_dataset, 
+                                                 med_sam=True,
+                                                 with_pix=True)
         p_record.append(p_record_vitb)
         b_record.append(b_record_vitb)
         pixel_count.append(pixel_count_vit)
         area_percentage.append(area_percentage_vit)
-        final_record = {"p":p_record,"b":b_record, "pixel_count":pixel_count,"area_percentage":area_percentage}
+        final_record = {"p":p_record, 
+                        "b":b_record, 
+                        "pixel_count":pixel_count,
+                        "area_percentage":area_percentage}
     with open(save_path, "wb") as f:
         pickle.dump(final_record, f)
 
@@ -70,3 +63,19 @@ for ckpt in ckpt_list:
     save_path_all = save_path + file_name[:-4]
 
     save_test_record(file_paths, sam_model, save_path_all)
+
+
+# def save_pxl_record(file_paths, save_path):
+#     pixel_count, area_percentage = [], []
+#     for file_path in file_paths:
+#         print("Processing the dataset: ",file_path)
+#         test_dataset = NiiDataset([file_path], multi_mask= True)
+#         pixel_count_vit, area_percentage_vit = get_pix_num_from_ds(test_dataset=test_dataset)
+#         pixel_count.append(pixel_count_vit)
+#         area_percentage.append(area_percentage_vit)
+#         final_record = {"pixel_count":pixel_count,"area_percentage":area_percentage}
+#     with open(save_path, "wb") as f:
+#         pickle.dump(final_record, f)
+
+# save_pxl_record(file_paths, save_path)
+# print("Done!")
