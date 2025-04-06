7cc9df6bcce09e263b485bcc583f2bd83b370576
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2025-04-06 21:44:07 +1000

    delete all masks whose pixels less than 10

--- image_processing/post_process_test_masks.py
+++ image_processing/post_process_test_masks.py
@@ -1,32 +1,37 @@
 import sys
 sys.path.append("..")
-import os
-join = os.path.join
 from tqdm import tqdm
 from utils.dataloader import *
 from utils.utils import *
-from utils.prompt import *
-from glob import glob
 
 file_paths = TEST_IMAGE_PATH
+
 test_dataset = NiiDataset(file_paths, multi_mask= True)
 
-count = 0
-save_label = False
-for embedding, mask, ori_size in (tqdm(train_dataset)):
-    file_name = train_dataset.cur_name
+def process_masks(mask, img_path, msk_path, threshold=10):
+    count = 0
+    save_label = False
     mask_labels = np.unique(mask)[(np.unique(mask).nonzero()[0])]
     for label in mask_labels:
-        if np.sum(mask==label) < 10:
+        if np.sum(mask==label) < threshold:
             count += 1
             mask[np.where(mask==label)] = 0
             save_label = True
     if np.sum(mask)==0:
-        os.remove(file_name)
+        os.remove(img_path)
+        os.remove(msk_path)
     elif save_label:
-        np.savez_compressed(file_name, img=embedding, mask=mask, ori_size=ori_size)
-        save_label = False
-print(f"Completed! {count} labels have been processed!")
-            
-def process_masks():
-    pass
+        new_mask = nib.Nifti1Image(mask, np.eye(4))
+        nib.save(new_mask, msk_path)
+    return count
+    
+if __name__ == "__main__":
+    count_all = 0
+    for _, mask in (tqdm(test_dataset)):
+        img_path = test_dataset.cur_img_name
+        msk_path = test_dataset.cur_msk_name
+        count_all += process_masks(mask, img_path, msk_path)
+    print(f"Completed! {count_all} labels have been processed!")
+        
+    
+                
