ea6ca9d11345751d7943479a7131013d5434e632
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2025-04-23 18:57:07 +1000

    delete the print content.

--- train_in_batch_multi_gpu.py
+++ train_in_batch_multi_gpu.py
@@ -144,7 +144,7 @@ def main(gpu, world_size, num_epochs, save_every):
 
         ## save the latest model
         if (epoch + 1) % save_every == 0 and gpu == 0:
-            print(f"The {epoch+1} / {num_epochs} epochs,  Loss: {epoch_loss/21:.4f}.")
+            print(f"The {epoch+1} / {num_epochs} epochs,  Loss: {epoch_loss:.4f}.")
             torch.save(samri_model.module.state_dict(), join(model_save_path, f"samri_vitb_box_{str(epoch+1)}.pth"))
             print(f"Checkpoint <samri_vitb_box_{str(epoch+1)}.pth> has been saved.")
     destroy_process_group()
