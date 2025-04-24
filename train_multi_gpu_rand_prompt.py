65c42109e42b72027aa664fd31632908c096e530
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2025-04-25 01:07:38 +1000

    change master address

--- train_multi_gpu_rand_prompt.py
+++ train_multi_gpu_rand_prompt.py
@@ -41,7 +41,7 @@ def ddp_setup(rank: int, world_size: int):
     rank: Unique identifier of each process
     world_size: Total number of processes
     """
-    os.environ["MASTER_ADDR"] = "localhost1"
+    os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = "29300"
     torch.cuda.set_device(rank)
     init_process_group(backend="nccl", rank=rank, world_size=world_size)
