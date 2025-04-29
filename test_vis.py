a025a4ca4781623755541e1f16015ff13a9bd989
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2025-04-29 10:31:12 +1000

    test samri

--- test_vis.py
+++ test_vis.py
@@ -5,19 +5,19 @@ from utils.utils import *
 
 file_paths = TEST_IMAGE_PATH
 ckpt_root_path = "/scratch/project/samri/Model_save/"
-model_folder = "from_medsam/"
-# model_folder = "box-501_balance/"
+# model_folder = "from_medsam/"
+model_folder = "box-501_balance/"
 save_path = "/scratch/project/samri/Eval_results/" + model_folder
 ckpt_list = [
             #  ckpt_root_path + model_folder + "samri_vitb_box_0.pth",
-            #  ckpt_root_path + model_folder + "samri_vitb_box_1.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_2.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_3.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_4.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_5.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_6.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_7.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_8.pth",
+             ckpt_root_path + model_folder + "samri_vitb_box_1.pth",
+            #  ckpt_root_path + model_folder + "samri_vitb_box_2.pth",
+            #  ckpt_root_path + model_folder + "samri_vitb_box_3.pth",
+            #  ckpt_root_path + model_folder + "samri_vitb_box_4.pth",
+            #  ckpt_root_path + model_folder + "samri_vitb_box_5.pth",
+            #  ckpt_root_path + model_folder + "samri_vitb_box_6.pth",
+            #  ckpt_root_path + model_folder + "samri_vitb_box_7.pth",
+            #  ckpt_root_path + model_folder + "samri_vitb_box_8.pth",
             #  ckpt_root_path + model_folder + "samri_vitb_box_9.pth",
             #  ckpt_root_path + model_folder + "samri_vitb_box_10.pth",
              ]
