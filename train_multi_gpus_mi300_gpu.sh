2f622605e90fe2ca84189ae4b101e8a081907b5c
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2025-05-23 16:38:17 +1000

    2gpus

---
+++ train_multi_gpus_mi300_gpu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=256G
+#SBATCH --job-name=SAMRI
+#SBATCH --time=7-00:00:00
+#SBATCH --partition=gpu_rocm
+#SBATCH --gres=gpu:mi300x:2
+#SBATCH --account=a_auto_mr_disease
+#SBATCH --qos=gpu
+#SBATCH -o /home/s4670484/Documents/slurm-%j.output
+#SBATCH -e /home/s4670484/Documents/slurm-%j.error
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=zhao.wang1@uq.edu.au
+
+module load anaconda3
+source $EBROOTANACONDA3/etc/profile.d/conda.sh
+conda activate samri-mi300
+
+python train_in_batch_multi_gpu.py
+
