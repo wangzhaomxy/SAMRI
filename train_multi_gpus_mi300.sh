62d3896b6735b62aa051a5c8cda321c8c7c2c36f
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2025-05-24 00:47:17 +1000

    using srun in 8 gpu tasks.

--- train_multi_gpus_mi300.sh
+++ train_multi_gpus_mi300.sh
@@ -23,6 +23,6 @@ export MASTER_ADDR=localhost
 export MASTER_PORT=$((26000 + RANDOM % 1000))  # Pick a port between 26000 ~ 26999
 
 amd-smi monitor
-python train_in_batch_multi_gpu.py
-# srun python train_multi_gpu_srun.py
+# python train_in_batch_multi_gpu.py
+srun python train_multi_gpu_srun.py
 
