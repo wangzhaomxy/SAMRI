586f0d2c1b114cf2d0ed6cf7df48c05bd3d3e0cb
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2025-05-23 23:00:14 +1000

    update the num_workers

--- train_multi_gpus_mi300_gpu.sh
+++ train_multi_gpus_mi300_gpu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash --login
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
-#SBATCH --cpus-per-task=64
-#SBATCH --mem=256G
+#SBATCH --cpus-per-task=96
+#SBATCH --mem=512G
 #SBATCH --job-name=SAMRI
 #SBATCH --time=7-00:00:00
 #SBATCH --partition=gpu_rocm
