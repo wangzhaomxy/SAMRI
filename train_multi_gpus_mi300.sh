410d700aa45abe2143c6471c8c4edc72bdcd41a7
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2025-05-25 23:00:51 +1000

    increase cpu ram to 1T instead of 768G to fix the OOM error.

--- train_multi_gpus_mi300.sh
+++ train_multi_gpus_mi300.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=64
-#SBATCH --mem=768G
+#SBATCH --mem=1T
 #SBATCH --job-name=SAMRI
 #SBATCH --time=7-00:00:00
 #SBATCH --partition=gpu_rocm
