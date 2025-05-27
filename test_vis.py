e950a5dc6b901f5a77d3a278667a583dcc368d57
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2025-05-27 15:25:23 +1000

    testing model

--- test_vis.py
+++ test_vis.py
@@ -9,7 +9,7 @@ ckpt_root_path = "/scratch/project/samri/Model_save/"
 model_folder = "fullds_balance_up_new_loss/"
 save_path = "/scratch/project/samri/Eval_results/" + model_folder
 ckpt_list = [
-             ckpt_root_path + model_folder + "samri_vitb_box_90.pth",
+             ckpt_root_path + model_folder + "samri_vitb_box_80.pth",
             #  ckpt_root_path + model_folder + "samri_vitb_box_70.pth",
             #  ckpt_root_path + model_folder + "samri_vitb_box_40.pth",
             #  ckpt_root_path + model_folder + "samri_vitb_box_30.pth",
