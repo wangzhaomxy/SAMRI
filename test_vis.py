5785f714b979d14720bb8421c16ad8350be5677f
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2025-09-03 04:16:14 +1000

    test all datasets for sam vitb model.

--- test_vis.py
+++ test_vis.py
@@ -3,38 +3,38 @@ from utils.visual import *
 from utils.utils import *
 import time
 
-TEST_ZERO_PATH = root_path + "Datasets/Zero_shot_val/"
-TEST_ZEROSHOT_PATH = [ds + "/validation/" for ds in sorted(glob(TEST_ZERO_PATH + "*"))]
-# file_paths = TEST_IMAGE_PATH + TEST_ZEROSHOT_PATH
+# TEST_ZERO_PATH = root_path + "Datasets/Zero_shot_val/"
+# TEST_ZEROSHOT_PATH = [ds + "/validation/" for ds in sorted(glob(TEST_ZERO_PATH + "*"))]
+file_paths = TEST_IMAGE_PATH + TEST_ZEROSHOT_PATH
 # file_paths = TEST_ZEROSHOT_PATH + VAL_ZEROSHOT_PATH + TRAIM_ZEROSHOT_PATH
-file_paths = TEST_ZEROSHOT_PATH
+# file_paths = TEST_ZEROSHOT_PATH
 ckpt_root_path = "/scratch/project/samri/Model_save/"
 model_folder = "bp_fullds_balance_up/"
 # model_folder = "fullds_balance_up_new_loss/"
-# save_path = "/scratch/project/samri/Eval_results/" + model_folder
-save_path = "/scratch/project/samri/Eval_results/" + "shoulder_bp_val/"
+save_path = "/scratch/project/samri/Eval_results/" + model_folder
+# save_path = "/scratch/project/samri/Eval_results/" + "shoulder_bp_val/"
 make_dir(save_path)
 
 # save_path1 = "/scratch/project/samri/Inference_results/" + "SAMRI/"
 # make_dir(save_path1)
 
-ckpt_list = [
-             ckpt_root_path + model_folder + "samri_vitb_box_1.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_2.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_3.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_4.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_5.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_6.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_7.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_8.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_9.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_10.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_20.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_30.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_40.pth",
-             ckpt_root_path + model_folder + "samri_vitb_box_50.pth",
-             ]
-# ckpt_list = ["/scratch/user/s4670484/Model_dir/sam_vit_b_01ec64.pth"]
+# ckpt_list = [
+#              ckpt_root_path + model_folder + "samri_vitb_box_1.pth",
+#              ckpt_root_path + model_folder + "samri_vitb_box_2.pth",
+#              ckpt_root_path + model_folder + "samri_vitb_box_3.pth",
+#              ckpt_root_path + model_folder + "samri_vitb_box_4.pth",
+#              ckpt_root_path + model_folder + "samri_vitb_box_5.pth",
+#              ckpt_root_path + model_folder + "samri_vitb_box_6.pth",
+#              ckpt_root_path + model_folder + "samri_vitb_box_7.pth",
+#              ckpt_root_path + model_folder + "samri_vitb_box_8.pth",
+#              ckpt_root_path + model_folder + "samri_vitb_box_9.pth",
+#              ckpt_root_path + model_folder + "samri_vitb_box_10.pth",
+#              ckpt_root_path + model_folder + "samri_vitb_box_20.pth",
+#              ckpt_root_path + model_folder + "samri_vitb_box_30.pth",
+#              ckpt_root_path + model_folder + "samri_vitb_box_40.pth",
+#              ckpt_root_path + model_folder + "samri_vitb_box_50.pth",
+#              ]
+ckpt_list = ["/scratch/user/s4670484/Model_dir/sam_vit_b_01ec64.pth"]
 # ckpt_list = ["/scratch/user/s4670484/Model_dir/sam_vit_h_4b8939.pth"]
 # ckpt_list = ["/scratch/user/s4670484/Model_dir/samri_vitb_box.pth"]
 # ckpt_list = ["/scratch/user/s4670484/Model_dir/samri_vitb_bp.pth"]
